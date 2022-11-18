285a1d82efbb0fad6ca7c8d47bcf834787d2efe0
raffal1993 <raffal1993@gmail.com>
2022-11-18 17:18:52 +0100

    prepare project to exercise 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { ProductsServiceModule } from './services/products.service-module';
-import { ProductListComponentModule } from './components/product-list/product-list.component-module';
-import { CategoriesServiceModule } from './services/categories.service-module';
-import { CategoriesComponentModule } from './components/categories/categories.component-module';
-import { CryptoServiceModule } from './services/crypto.service-module';
-import { CryptoComponentModule } from './components/crypto/crypto.component-module';
-import { PublicHolidaysComponentModule } from './components/public-holidays/public-holidays.component-module';
-import { PublicHolidaysServiceModule } from './services/public-holidays.service-module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    ProductsServiceModule,
-    ProductListComponentModule,
-    CategoriesServiceModule,
-    CategoriesComponentModule,
-    CryptoServiceModule,
-    CryptoComponentModule,
-    PublicHolidaysComponentModule,
-    PublicHolidaysServiceModule,
-  ],
+  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/models/product.model.ts
+++
@@ -1,8 +0,0 @@
-export interface ProductModel {
-  readonly id: number;
-  readonly title: string;
-  readonly price: string;
-  readonly category: string;
-  readonly description: string;
-  readonly image: string;
-}
