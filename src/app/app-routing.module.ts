f17ca048b0530615d120b971cabb5ddea3a93ad3
raffal1993 <raffal1993@gmail.com>
2022-11-18 19:23:19 +0100

    feat: create employee form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductComponent } from './components/product/product.component';
+import { EmployeeComponent } from './components/employee/employee.component';
 import { ProductComponentModule } from './components/product/product.component-module';
 import { ProductServiceModule } from './services/product.service-module';
+import { EmployeeComponentModule } from './components/employee/employee.component-module';
+import { EmployeeServiceModule } from './services/employee.service-module';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
       { path: 'create-product', component: ProductComponent },
+      { path: 'create-employee', component: EmployeeComponent },
     ]),
     ProductComponentModule,
     ProductServiceModule,
+    EmployeeComponentModule,
+    EmployeeServiceModule,
   ],
   exports: [RouterModule],
 })
