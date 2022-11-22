27bd7cec60ff66e9bbeb35025e1b6e04f2d5619c
raffal1993 <raffal1993@gmail.com>
2022-11-22 13:33:18 +0100

    feat: age prediction created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { CatFactComponent } from './components/cat-fact/cat-fact.component';
+import { AgePredictionComponent } from './components/age-prediction/age-prediction.component';
 import { ProductDetailComponentModule } from './components/product-detail/product-detail.component-module';
 import { ProductServiceModule } from './services/product.service-module';
 import { CatFactComponentModule } from './components/cat-fact/cat-fact.component-module';
 import { CatFactServiceModule } from './services/cat-fact.service-module';
+import { AgePredictionComponentModule } from './components/age-prediction/age-prediction.component-module';
+import { AgePredictionServiceModule } from './services/age-prediction.service-module';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
       { path: 'product/:id', component: ProductDetailComponent },
       { path: 'cat-fact', component: CatFactComponent },
+      { path: 'age/:name', component: AgePredictionComponent },
     ]),
     ProductDetailComponentModule,
     ProductServiceModule,
     CatFactComponentModule,
     CatFactServiceModule,
+    AgePredictionComponentModule,
+    AgePredictionServiceModule,
   ],
   exports: [RouterModule],
 })
---
+++ src/app/components/age-prediction/age-prediction.component.ts
@@ -0,0 +1,28 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { AgePredictionService } from '../../services/age-prediction.service';
+import { AgePredictionModel } from '../../models/age-prediction.model';
+
+@Component({
+  selector: 'app-age-prediction',
+  templateUrl: './age-prediction.component.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AgePredictionComponent {
+  readonly age$: Observable<AgePredictionModel> =
+    this._activatedRoute.params.pipe(
+      switchMap((data) => this._agePredictionService.getAge(data['name']))
+    );
+
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private _agePredictionService: AgePredictionService
+  ) {}
+}
