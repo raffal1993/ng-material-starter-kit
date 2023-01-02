348d8d38d32e6761938302f8a568740a02915173
raffal1993 <raffal1993@gmail.com>
2023-01-02 17:12:37 +0100

    sort + search + filter

---
+++ src/app/components/products-alter/products-alter.component-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { ProductsAlterComponent } from './products-alter.component';
+
+@NgModule({
+  imports: [],
+  declarations: [ProductsAlterComponent],
+  providers: [],
+  exports: [ProductsAlterComponent]
+})
+export class ProductsAlterComponentModule {
+}
