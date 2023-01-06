184e9ff7a1a07cb0dbefdcd8dea92dbecce8a63d
raffal1993 <raffal1993@gmail.com>
2023-01-06 13:12:23 +0100

    feat: beer paginator

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductsComponent } from './components/products/products.component';
+import { BeersComponent } from './components/beers/beers.component';
 import { ProductsComponentModule } from './components/products/products.component-module';
+import { BeersComponentModule } from './components/beers/beers.component-module';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
       { path: 'route-limit-single-products', component: ProductsComponent },
+      { path: 'route-pagination-beers', component: BeersComponent },
     ]),
     ProductsComponentModule,
+    BeersComponentModule,
   ],
   exports: [RouterModule],
 })
--- src/app/components/beers/beers.component-module.ts
+++ src/app/components/beers/beers.component-module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatChipsModule } from '@angular/material/chips';
 import { BeersComponent } from './beers.component';
 
 @NgModule({
-  imports: [MatCardModule],
+  imports: [MatCardModule, CommonModule, MatPaginatorModule, MatChipsModule],
   declarations: [BeersComponent],
   providers: [],
   exports: [BeersComponent],
 })
-export class BeersComponentModule {}
+export class BeersComponentModule { }
