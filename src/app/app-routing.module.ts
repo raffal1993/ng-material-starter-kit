a30ba4b2f5cbc454cb3c1358e3583af68c991147
raffal1993 <raffal1993@gmail.com>
2023-01-12 11:11:17 +0100

    clean project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NamesComponent } from './components/names/names.component';
-import { NamesComponentModule } from './components/names/names.component-module';
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot([{ path: 'query-loader-names', component: NamesComponent }]),
-    NamesComponentModule,
-  ],
+  imports: [RouterModule.forRoot([])],
   exports: [RouterModule],
 })
 // @ts-ignore
